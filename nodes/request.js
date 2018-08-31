c8a213564ac835e1237b269a5372b93b12df0305
Dean Mauro <selmerace@gmail.com>
2018-08-31 18:07:27 -0400

    Removed request node's ability to read from input (would never be used anyway), greatly simplified verbose code, moved common node functions to a Utilities class, updated field icons, changed Start Job fields to typedInputs

--- nodes/request.js
+++ nodes/request.js
@@ -1,4 +1,5 @@
 const Api = require('../lib/api.js');
+const Utilities = require('../lib/utilities.js');
 
 module.exports = function(RED) {
     function OrchestratorRequestNode(config) {
@@ -8,109 +9,53 @@ module.exports = function(RED) {
 
             var node = this;
             var connection = RED.nodes.getNode(config.connection);
-            var body;
+            var body = {};
             var callback = function(x, status) { 
                                         if (status < 300) node.send({payload: x});
                                         else node.error(x);
                                     };
 
-            // Refresh token if needed. Error out if not found.
-            if (!connection)
-                throw "Please add Login credentials before making requests.";
-            
+            // Ensure node has connection
+            Utilities.checkConnection(connection);
 
             // Properties Input
-            if (config.category != "UseInput") {
-                try {
-                    // Convert fields provided through msg variable
-                    if (config.category.startsWith("Msg")) {
-                        config.category = msg.payload.category;
-                        config.action = msg.payload.action;
-                    }
-
-                    // Select Params
-                    if (config.params.length != 0)
-                        body = convertParams(config.params, msg, node);
-                    else if (msg.payload.params)
-                        body = msg.payload.params;
-                    else
-                        body = {};
-
-                    // Check that a category and action were specified
-                    if (!config.category) throw "That request was rather vague. Please specify a category in the node's properties."
-                    if (!config.action) throw "That request was rather vague. Please specify an action in the node's properties."
-
-                    // Get endpoint info
-                    var endpoint = Api[config.category](config.action);
-
-                    // Add path & query params if needed
-                    var extension = Api.fillPath(endpoint[0], endpoint[1], body);
-
-                    // Sanitize body
-                    if (body && body["Id"]) body["Id"] = parseInt(body["Id"]);
-
-                    // Fire!
-                    connection.request({ type: endpoint[0], 
-                                         extension: extension,
-                                         body: JSON.stringify(body),
-                                         callback: callback });
-                } catch(e) {
-                    this.error(e);
+            try {
+                // Convert fields provided through msg variable
+                if (config.category.startsWith("Msg")) {
+                    config.category = msg.payload.category;
+                    config.action = msg.payload.action;
                 }
-            }
 
-            // JSON Input
-            else if (msg.payload.action && msg.payload.extension) {
-                
-                connection.request({ type: msg.payload.action, 
-                                     extension: msg.payload.extension,
-                                     body: JSON.stringify(msg.payload.body) || "",
+                // Select Params
+                if (config.params.length != 0)
+                    body = Utilities.convertParams(config.params, msg, node);
+                else if (msg.payload.params)
+                    body = msg.payload.params;
+
+                // Check that a category and action were specified
+                if (!config.category) throw "That request was rather vague. Please specify a category in the node's properties."
+                if (!config.action) throw "That request was rather vague. Please specify an action in the node's properties."
+
+                // Get endpoint info
+                var endpoint = Api[config.category](config.action);
+
+                // Add path & query params if needed
+                var extension = Api.fillPath(endpoint[0], endpoint[1], body);
+
+                // Sanitize body
+                if (body && body["Id"]) body["Id"] = parseInt(body["Id"]);
+
+                // Fire!
+                connection.request({ type: endpoint[0], 
+                                     extension: extension,
+                                     body: JSON.stringify(body),
                                      callback: callback });
-            } 
-
-            // Bad Input
-            else {
-                this.error("Bad input. Please refer to the info tab for formatting.");
+            } catch(e) {
+                this.error(e);
             }
+
         });
     }
 
-
-    function convertParams(params, msg, node) {
-        var body = {};
-        
-        for (var p of params) {
-            var val = p.value;
-
-            switch(p.type) {
-                case 'msg':
-                    val = RED.util.getMessageProperty(msg, "msg." + p.value); break;
-                case 'json':
-                    val = JSON.parse(p.value); break;
-                case 'flow':
-                    val = node.context().flow.get(p.value); break;
-                case 'global':
-                    val = node.context().global.get(p.value); break;
-                case 'date':
-                    val = Date.now(); break;
-                case 'jsonata':
-                    try {
-                        var prep = RED.util.prepareJSONataExpression(p.value, msg);
-                        val = RED.util.evaluateJSONataExpression(prep, msg);
-                    } catch(err) {
-                        throw "Invalid JSONata expression";
-                    }
-                    break;
-            }
-
-            body[p.key] = val;
-        }
-
-        return body;
-    }
-
-    
-    
-    
     RED.nodes.registerType("orchestrator request", OrchestratorRequestNode);
 }
--- nodes/startJob.js
+++ nodes/startJob.js
@@ -1,4 +1,5 @@
 const Api = require('../lib/api.js');
+const Utilities = require('../lib/utilities.js');
 
 module.exports = function(RED) {
     function OrchestratorStartJobNode(config) {
@@ -6,87 +7,35 @@ module.exports = function(RED) {
 
         this.on('input', function(msg) {
 
-            var refreshToken = this.context().flow.get("refreshToken");
             var node = this;
-            var body;
+            var connection = RED.nodes.getNode(config.connection);
+            var body = {};
             var callback = function(x, status) { 
                                         if (status < 300) node.send({payload: x});
-                                        else errorOut(node, x);
+                                        else node.error(x);
                                     };
 
-            // Refresh token if needed. Error out if not found.
-            if (refreshToken) refreshToken();
-            else errorOut(node, "Please add a Login node to the flow before making requests.");
-            
-            // Use orch object to make calls
-            var orch = this.context().flow.get("orch");
+            // Ensure node has connection
+            Utilities.checkConnection(connection);
 
             // Properties Input
             try {
                 // Select Params
                 if (config.params.length != 0)
-                    body = convertParams(config.params, msg, node);
-                else if (msg.payload.params)
-                    body = msg.payload.params;
-                else
-                    body = {};
+                    body = Utilities.convertParams(config.params, msg, node);
 
                 // Get endpoint info
                 var endpoint = Api["Jobs"]("StartJobs");
 
                 // Fire!
-                orch.request({ type: endpoint[0], 
-                               extension: extension,
-                               body: JSON.stringify(body),
-                               callback: callback });
+                connection.request({ type: endpoint[0], 
+                                     extension: extension,
+                                     body: JSON.stringify(body),
+                                     callback: callback });
             } catch(e) {
                 errorOut(node, e);
             }
         });
-    }
-
-
-    function convertParams(params, msg, node) {
-        var body = {};
-        
-        for (var p of params) {
-            var val = p.value;
-
-            switch(p.type) {
-                case 'msg':
-                    val = RED.util.getMessageProperty(msg, "msg." + p.value); break;
-                case 'json':
-                    val = JSON.parse(p.value); break;
-                case 'flow':
-                    val = node.context().flow.get(p.value); break;
-                case 'global':
-                    val = node.context().global.get(p.value); break;
-                case 'date':
-                    val = Date.now(); break;
-                case 'jsonata':
-                    try{
-                        var prep = RED.util.prepareJSONataExpression(p.value, msg);
-                        val = RED.util.evaluateJSONataExpression(prep, msg);
-                    } catch(err) {
-                        errorOut(node, "Invalid JSONata expression");
-                        return;
-                    }
-                    break;
-            }
-
-            body[p.key] = val;
-        }
-
-        return body;
-    }
-
-    
-    function errorOut(node, error) {
-        node.error(error);
-        node.send({ result: null,
-                    success: false,
-                    error: { message: "Error. Can't proceed." }
-                 });
     }
     
     RED.nodes.registerType("orchestrator start job", OrchestratorStartJobNode);
